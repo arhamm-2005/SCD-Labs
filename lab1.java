026d0cec937608718782bd89d8ef104aa2c310dd
Muhammad Arham Shakeel <153810088+arhamm-2005@users.noreply.github.com>
2025-10-27 15:50:02 +0500

    Add additional output to lab1

--- lab1.java
+++ lab1.java
@@ -1,5 +1,7 @@
 public class lab1{
     public static void main(String[] args){
         System.out.println("2023F-BSE-089 SCD lab1");
+        System.out.println("2023F-BSE-089 SCD lab1 Arham");
     }
+
 }
